adb66a8975fa722c3527455c5fbf1b12ddcbca39
Catlak Profesor <progesor@gmail.com>
2026-07-26 00:02:17 +0300

    fix(m3): export currency-aware sync database

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,7 @@
+export * from './currency-code-database.ts';
 export * from './database.ts';
 export * from './reporting-database.ts';
 export * from './schema.ts';
 export * from './sync-database.ts';
+export { SyncDatabase } from './currency-aware-sync-database.ts';
 export * from './sync-recovery.ts';
