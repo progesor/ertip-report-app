aa1989cc19739c0e3f8e42f2103872bf5cd7796e
Catlak Profesor <progesor@gmail.com>
2026-07-25 18:36:33 +0300

    fix(odoo): expose resilient tenant discovery

--- packages/odoo-client/src/index.ts
+++ packages/odoo-client/src/index.ts
@@ -1,2 +1,18 @@
 export * from './client.ts';
-export * from './discovery.ts';
+export {
+  TENANT_DISCOVERY_MODELS,
+  type TenantDiscoveryCompany,
+  type TenantDiscoveryDateSemantics,
+  type TenantDiscoveryField,
+  type TenantDiscoveryMissingValueCount,
+  type TenantDiscoveryModel,
+  type TenantDiscoveryModelName,
+  type TenantDiscoveryResult,
+  type TenantDiscoverySelectionValue,
+  type TenantDiscoveryStateCount,
+  type TenantDiscoveryUserCandidate,
+} from './discovery.ts';
+export {
+  discoverOdooTenant,
+  repairTenantDiscoveryStateCounts,
+} from './discovery-resilient.ts';
