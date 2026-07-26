44a3c124e9eb56a420698394a7b60980c1051577
Catlak Profesor <progesor@gmail.com>
2026-07-26 15:00:18 +0300

    feat(m5): export currency-aware database reports

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,6 +3,7 @@ export * from './database.ts';
 export * from './quotation-conversion-database.ts';
 export * from './reporting-database.ts';
 export * from './schema.ts';
+export * from './source-currency-reporting-database.ts';
 export * from './sync-database.ts';
 export { SyncDatabase } from './currency-aware-sync-database.ts';
 export * from './sync-recovery.ts';
